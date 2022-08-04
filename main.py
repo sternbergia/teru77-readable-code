4d0e6044accbd87f2d17ce1ea8e2a62e0617d720
teru77 <teru-ngmr0715@docomo.ne.jp>
2022-08-04 15:54:23 +0900

    ファイルの分離

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("上手")
+import csv
+word = "上手"
+
+filename = "word.csv"
+with open(filename, encoding="utf8", newline="") as f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        print(*row)
