4cd3a274b4545aed8ad2843baddc32f34cb36208
y.kuriki <kuriki.yui@gmail.com>
2022-08-05 14:26:25 +0900

    spec7: 仕様７を追加

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import csv
+
+
 def main():
     filePath = "./words.csv"
     input_num = input("数字を入力してください：")
-
     with open(filePath, encoding="utf8", newline="") as f:
         words_road = csv.reader(f)
-        if not input_num:
+        if True:  # 入力に関係なくこちらを処理する
             for i, word in enumerate(words_road):
-                print(f"{i+1}:",*word)
+                print(f"{i+1}:", word[0], word[1])
         else:
             for i, word in enumerate(words_road):
-                if (i+1 == int(input_num)):
-                    print(f"{i+1}:",*word)
+                if i + 1 == int(input_num):
+                    print(f"{i+1}:", *word)
                     break
+
 
 if __name__ == "__main__":
     main()
