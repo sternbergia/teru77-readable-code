5e13e0abe0455f04b0cd7a6347eb367081943593
shwg8986 <sho8986go@gmail.com>
2022-08-05 13:37:20 +0900

    spec6 インプットの処理を追加

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import csv
 def main():
-    filePath = "./word.csv"
-    id_num = 1
+    filePath = "./words.csv"
+    input_num = input("数字を入力してください：")
 
     with open(filePath, encoding="utf8", newline="") as f:
-        rows = csv.reader(f)
-        for row in rows:
-            print(f"{id_num}:",*row)
-            id_num=id_num+1
+        words_road = csv.reader(f)
+        if not input_num:
+            for i, word in enumerate(words_road):
+                print(f"{i+1}:",*word)
+        else:
+            for i, word in enumerate(words_road):
+                if (i+1 == int(input_num)):
+                    print(f"{i+1}:",*word)
+                    break
 
 if __name__ == "__main__":
     main()
