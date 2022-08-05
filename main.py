b89510f740c59df1c3b429bdcb05f5bfc05e3be4
teru77 <teru-ngmr0715@docomo.ne.jp>
2022-08-05 13:25:41 +0900

    spec5: 変数csvreaderをrowsに変更

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import csv
 def main():
     filePath = "./word.csv"
     id_num = 1
+
     with open(filePath, encoding="utf8", newline="") as f:
-        csvreader = csv.reader(f)
-        for row in csvreader:
+        rows = csv.reader(f)
+        for row in rows:
             print(id_num,*row)
             id_num=id_num+1
+            
 if __name__ == "__main__":
     main()
