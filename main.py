cd3fd4cda24546eb170acc6c7ee6d592d88dba6b
teru77 <teru-ngmr0715@docomo.ne.jp>
2022-08-05 13:23:22 +0900

    spec5: IDの追加

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import csv
-
 def main():
-
-    filePath = './word.csv'
-    
+    filePath = "./word.csv"
+    id_num = 1
     with open(filePath, encoding="utf8", newline="") as f:
         csvreader = csv.reader(f)
         for row in csvreader:
-            print(*row)
-
-if __name__ == '__main__':
+            print(id_num,*row)
+            id_num=id_num+1
+if __name__ == "__main__":
     main()
